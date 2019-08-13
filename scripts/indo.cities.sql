a252b0a46f02388b87a96443bc3d5019042033cb
Eddy O'Donnell <eddyod@yahoo.com>
2019-08-13 09:04:08 -0700

    Added a csv file of cities/lat/long

---
+++ scripts/indo.cities.sql
@@ -0,0 +1,11 @@
+
+
+update location set address="Sukabumi Indonesia", latitude = -6.9096, longitude =  106.9000 where id = 1;
+update location set address="Surakarta Indonesia", latitude = -7.5650, longitude =  110.8250 where id = 3;
+update location set address="Bogor Indonesia", latitude = -6.5700, longitude =  106.7500 where id = 4;
+update location set address="Magelang Indonesia", latitude = -7.4696, longitude =  110.1800 where id = 5;
+update location set address="Majene Indonesia", latitude = -3.5336, longitude =  118.9660 where id = 6;
+update location set address="Ende Indonesia", latitude = -8.8623, longitude =  121.6489 where id = 7;
+update location set address="Maumere Indonesia", latitude = -8.6189, longitude =  122.2123 where id = 8;
+update location set address="Kediri Indonesia", latitude = -7.7896, longitude =  112.0000 where id = 9;
+update location set address="Sibolga Indonesia", latitude = 1.7500, longitude =  98.8000 where id = 10;
